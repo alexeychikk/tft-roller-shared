1284a2dd6764b6d402a2093a5c909f32cd94721e
Alex Zinkevych <alexdarkmm@gmail.com>
2024-02-12 00:50:53 +0200

    chore: shared utils for env service

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './capitalize';
 export * from './combinatorics';
 export * from './comps';
+export * from './getClassGetters';
 export * from './validation';
 export * from './weightedRandom';
